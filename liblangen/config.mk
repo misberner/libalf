30dfb222fb1fa314138724cf0a8cce17f92c791e
Malte Isberner <malte.isberner@gmail.com>
2015-03-18 10:52:54 +0100

    Enabled Mac OS X and clang compileability for finite automata tool and liblangen

--- liblangen/config.mk
+++ liblangen/config.mk
@@ -13,3 +13,38 @@ BINDIR?=${PREFIX}/bin
 SHAREDIR?=${PREFIX}/share
 DOCDIR?=${SHAREDIR}/doc/liblangen
 
+ifndef OS
+	OS=$(shell uname -s)
+endif
+
+ifeq (${OS}, Windows_NT) # Windows
+	LIBEXT=dll
+	LDFLAGS+=-lws2_32
+else ifeq (${OS}, Darwin) # Mac OS
+	LIBEXT=dylib
+else ifeq (${OS}, Linux) # Linux
+	LIBEXT=so
+else
+	$(error Unsupported operating system ${OS})
+endif
+
+# Skip lib prefix on Windows
+ifeq (${OS}, Windows_NT)
+	LIBPREFIX=
+else
+	LIBPREFIX=lib
+endif
+
+# Use ginstall in Mac OS X
+ifeq (${OS}, Darwin)
+	INSTALL?=ginstall
+else
+	INSTALL?=install
+endif
+
+# ldconfig needs to be run on Linux
+ifeq (${OS}, Linux)
+	RUN_LDCONFIG=test `id -u` -eq 0 && /sbin/ldconfig
+else
+	RUN_LDCONFIG=
+endif
